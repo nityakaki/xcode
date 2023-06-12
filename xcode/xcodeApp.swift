be5dd22457bdf3c0d12ce4c4bfc103858e9fee04
Nidhi Kaki <kakinidhi1@gmail.com>
2023-06-12 11:05:01 -0700

    Initial Commit

---
+++ xcode/xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeApp.swift
+//  xcode
+//
+//  Created by nitya kaki  on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
